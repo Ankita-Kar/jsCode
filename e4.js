7286d230a671f5018a9d2ec440bcf250bf9f8b51
Ankita Kar <akar@argusoft.com>
2023-10-04 15:01:13 +0530

    new commit

--- e4.js
+++ e4.js
@@ -1,18 +1,31 @@
 const btn = document.querySelector('button');
 
-btn.addEventListener('click', function() {
-	alert('you clicked!!!!!');
-});
+// btn.addEventListener('click', function() {
+// 	alert('you clicked!!!!!');
+// });
 
-btn.addEventListener('click', function() {
-	console.log('second time!!');
-});
+// btn.addEventListener('click', function() {
+// 	console.log('second time!!');
+// });
+
+// btn.addEventListener('mouseover', function() {
+// 	btn.innerText = 'stop touching';
+// });
+
+// btn.addEventListener('mouseout', function() {
+// 	btn.innerText = 'Click Me!';
+// });
 
 btn.addEventListener('mouseover', function() {
-	btn.innerText = 'stop touching';
-});
+	console.log("moused over!!");
 
-btn.addEventListener('mouseout', function() {
-	btn.innerText = 'Click Me!';
-});
+	const height = Math.floor(Math.random() * window.innerHeight);
+	const width = Math.floor(Math.random() * window.innerWidth);
 
+	btn.style.top = `${height}px`
+	btn.style.left = `${width}px`
+});
+btn.addEventListener('click', function() {
+	btn.innerText = "Congrats you clicked me!"
+	document.body.style.backgroundColor = "green";
+});
