1c948e8f97ea99e9807b8a6dcff8067d98a8c740
Ankita Kar <akar@argusoft.com>
2023-10-04 10:42:10 +0530

    new commit

---
+++ e3(console).js
@@ -0,0 +1,14 @@
+//getELementByTagName
+document.querySelector('li');
+//getElementByClassName
+document.querySelector('.special');
+//getELementById
+document.querySelector('#main');
+//selecting more specific content of the pge if more than one exists for the same details
+document.querySelector('section ul li.special');
+
+//select all li
+document.querySelectorAll('li');
+
+//select all with class special
+document.querySelectorAll('.special');
