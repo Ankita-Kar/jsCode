31b57928bedbe96efdbc457ec5e63552a63d32fc
Ankita Kar <akar@argusoft.com>
2023-10-07 11:17:35 +0530

    new changes

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -7,9 +7,25 @@
 //   req.open("GET", "http://www.example.org/example.txt");
 //   req.send();
 
+//   const firstRequest = new XMLHttpRequest();
+//   firstRequest.addEventListener('load', () => {
+//     console.log("WORKED");
+//   });
+//   firstRequest.addEventListener('error', () => {
+//     console.log("FAILED");
+//   });
+
+//   firstRequest.open("GET", "https://swapi.dev/api/planets/");
+// firstRequest.send();
+// console.log("request sent")
+
   const firstRequest = new XMLHttpRequest();
-  firstRequest.addEventListener('load', () => {
+  firstRequest.addEventListener('load', function () {
     console.log("WORKED");
+    const data = JSON.parse(this.responseText);
+for(let planet of data.results){
+    console.log(planet.name);
+}
   });
   firstRequest.addEventListener('error', () => {
     console.log("FAILED");
